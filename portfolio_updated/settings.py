2820c02edfeb73d4f6b0ed92fb72869671c7d210
samraj <61903698+samrajkodai@users.noreply.github.com>
2022-07-21 10:22:40 +0530

    database

--- portfolio_updated/settings.py
+++ portfolio_updated/settings.py
@@ -103,10 +103,15 @@ WSGI_APPLICATION = 'portfolio_updated.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'zVCBbzxYyjyfLewJ4vyF',
+        'HOST': 'containers-us-west-67.railway.app',
+        'PORT': '5917',
     }
 }
 
